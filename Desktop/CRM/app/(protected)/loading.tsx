fa0670ef3652eca5ba293a5ba4c56be47b58a642
soofitraders <soofitraders25@gmail.com>
2025-12-02 15:45:06 +0500

    Delete Desktop/CRM directory

--- Desktop/CRM/app/(protected)/loading.tsx
+++
@@ -1,6 +0,0 @@
-import PageLoader from '@/components/ui/PageLoader'
-
-export default function Loading() {
-  return <PageLoader />
-}
-
--- Desktop/CRM/lib/utils/pdfkit-polyfill.ts
+++
@@ -1,3 +0,0 @@
-// Empty polyfill - not needed for server-side rendering
-// PDFKit works fine without this polyfill in Next.js serverless functions
-export {}
